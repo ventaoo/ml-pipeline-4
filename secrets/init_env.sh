c7fd20a78dfd92e09df4ce95917846f720792c1d
zwttt <zwt@zhuwentaodeMacBook-Air.local>
2025-03-18 20:44:51 +0400

    Fixed the env load

--- secrets/init_env.sh
+++ secrets/init_env.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# 读取解密后的文件内容并导出为环境变量
-export DB_HOST=$(jq -r '.db_host' /shared/db_info.json)
-export DB_PORT=$(jq -r '.db_port' /shared/db_info.json)
-export DB_NAME=$(jq -r '.db_name' /shared/db_info.json)
-export DB_USER=$(jq -r '.db_user' /shared/db_info.json)
-export DB_PASSWORD=$(jq -r '.db_password' /shared/db_info.json)
+export $(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' /shared/db_info.json)
