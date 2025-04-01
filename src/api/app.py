296e50677f8fa0626015d73f0f2e0baf39ae33f1
ventaoo <ventaoczu@gmail.com>
2025-04-01 15:48:58 +0400

    Add the kafka func producer and consumer

--- src/api/app.py
+++ src/api/app.py
@@ -1,12 +1,18 @@
 import configparser
 import os
 import sys
+import json
 import joblib
+from datetime import datetime
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from gensim.models import Word2Vec
 from dotenv import load_dotenv
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from threading import Thread
+from contextlib import asynccontextmanager
 
 sys.path.insert(1, os.path.join(os.getcwd(), "src"))
 
@@ -22,8 +28,17 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# 创建 FastAPI 应用
-app = FastAPI()
+# 定义 Consumer 后台任务
+def consume_messages():
+    consumer = KafkaConsumer(
+        "model-results",
+        bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092"),
+        auto_offset_reset="earliest",
+        value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+    )
+    for message in consumer:
+        print(f"Consumer 收到消息: {message.value}")
+        # 在此处添加业务逻辑（如保存到数据库）
 
 # 定义数据输入格式
 class InputData(BaseModel):
@@ -52,6 +67,28 @@ class api_():
         prediction = self.model.predict(input_data)
         return int(prediction)  # 确保返回整数
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 服务器启动!")
+     # 从环境变量读取 Kafka 配置（示例值：kafka:9092）
+    app.kafka_producer = KafkaProducer(
+        bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092"),
+        value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    )
+    print('Kafka producer 启动!')
+
+    # 启动 Consumer 线程
+    Thread(target=consume_messages, daemon=True).start()
+    print('Kafka consumer 启动!')
+
+    yield
+    
+    app.kafka_producer.close()
+    print("🛑 服务器关闭!")
+
+# 创建 FastAPI 应用
+app = FastAPI(lifespan=lifespan)
+
 # 预测 API 实例
 api_interface = api_()
 
@@ -62,5 +99,13 @@ def predict(data: str):
     
     # 存入数据库时存储 (输入文本, 预测结果)
     store_prediction_results([(data, prediction)], "predictions")  
+
+    # 发送消息到 Kafka
+    message = {
+        "input": data,
+        "prediction": prediction,
+        "timestamp": datetime.now().isoformat()
+    }
+    app.kafka_producer.send("model-results", value=message)
     
     return {"input": data, "prediction": prediction}
